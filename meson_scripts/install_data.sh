06f9101cd26e2814c388cc9735536f34208c24f1
Sergio Costas <rastersoft@gmail.com>
2026-02-21 20:39:12 +0100

    Update version number

--- meson_scripts/install_data.sh
+++ meson_scripts/install_data.sh
@@ -3,7 +3,7 @@
 shopt -s extglob
 mkdir -p $DESTDIR/$1
 if [ -d "$2" ]; then
-	cp -a $2/!(CMakeLists.txt) $DESTDIR/$1
+	cp -a $2/ $DESTDIR/$1 || true
 else
-	cp -a $2 $DESTDIR/$1
+	cp -a $2 $DESTDIR/$1 || true
 fi
